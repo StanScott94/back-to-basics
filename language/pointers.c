491df0ec69d350e228e2965cbdf8e4971ab71e94
StanScott94 <twinstanton@gmail.com>
2024-03-17 01:01:20 +0100

    examples

---
+++ language/pointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 10;
+    int *y = &x;
+    int **z = &y;
+    
+    printf("value of int x = %d\n", x);
+    printf("address of int x = %p\n", (void *)&x);
+    printf("value of pointer y = %p\n", (void *)y);
+    printf("address of pointer y = %p\n", (void *)&y);
+    printf("dereferenced pointer y = %d\n", *y);
+    printf("value of pointer to pointer z = %p\n", (void *)z);
+    printf("address of pointer to pointer z = %p\n", (void *)&z);
+    printf("dereferenced pointer z = %p\n", (void *)*z);
+    printf("dereferenced pointer to pointer z = %d\n", **z);
+}
